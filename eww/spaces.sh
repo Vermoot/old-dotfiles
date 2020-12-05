aa27bdb5b6f96ddac84ae4c0f9247c4ad6968294
Vermoot <vermoot@gmail.com>
2020-12-05 19:38:06 +0100

    beautified spaces.sh

--- eww/spaces.sh
+++ eww/spaces.sh
@@ -1,21 +1,20 @@
 spaces_query=$(yabai -m query --spaces --display)
+windows=true
 output="<box class='aaaa'>"
 for space in $(echo $spaces_query | jq -c '.[]')
 do
 	if [ $(echo $space | jq .visible) = 1 ]
 	then
-		output+="
-		<button class='visible-space'>
-			$(echo $space | jq .index)
-		</button>
-		"
+		button_properties="class='visible-space'"
 	else
-		output+="
-		<button class='space' onclick='yabai -m space --focus $(echo $space | jq .index)'>
-			$(echo $space | jq .index)
-		</button>
-		"
+		button_properties="class='space' onclick='yabai -m space --focus $(echo $space | jq .index)'"
 	fi
+
+	output+="
+	<button $button_properties>
+		$(echo $space | jq .index)
+	</button>
+	"
 done
 output+="</box>"
 echo $output
