0ef5d684d4115b1e2c96ead7dd873c1d10ddffcf
Vermoot <vermoot@gmail.com>
2020-12-09 15:36:01 +0100

    make the space a label if it's active, instead of a button

--- eww/spaces.sh
+++ eww/spaces.sh
@@ -1,20 +1,16 @@
 spaces_query=$(yabai -m query --spaces --display)
 windows=true
-output="<box class='aaaa'>"
+output="<box>"
 for space in $(echo $spaces_query | jq -c '.[]')
 do
+	index="$(echo $space | jq .index)"
 	if [ $(echo $space | jq .visible) = 1 ]
 	then
-		button_properties="class='visible-space'"
+		output+="<label class='visible-space' text='$index'/>"
 	else
-		button_properties="class='space' onclick='yabai -m space --focus $(echo $space | jq .index)'"
+		output+="<button class='space' onclick='yabai -m space --focus $index'>$index</button>"
 	fi
 
-	output+="
-	<button $button_properties>
-		$(echo $space | jq .index)
-	</button>
-	"
 done
 output+="</box>"
 echo $output
