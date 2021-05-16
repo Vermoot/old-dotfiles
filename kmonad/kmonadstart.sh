f6969fcb8e4d8dcc693ead12f23311ecf294a5e4
Vermoot <vermoot@gmail.com>
2021-05-16 16:35:14 +0200

    update kmonad config with colemak wide and more home row mods

---
+++ kmonad/kmonadstart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/sbin/kextload /Users/vermoot/Projects/kmonad/c_src/mac/Karabiner-DriverKit-VirtualHIDDevice/dist/Karabiner-DriverKit-VirtualHIDDevice-1.15.0.pkg
+/usr/local/bin/kmonad /Users/vermoot/.config/kmonad/applekeeb.kbd
